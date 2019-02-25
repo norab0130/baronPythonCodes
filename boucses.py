c5881dfbd31642df23f3ae9e6d849b89a0b58f57
aaronliu0130 <aaronliu0130@outlook.com>
2019-02-25 17:28:07 +0800

    Update boucses.py

--- boucses.py
+++ boucses.py
@@ -2,7 +2,17 @@
  Aaron
 '''
 import pygame
-import random
 pygame.init()
-    
+screen = pygame.display.set_mode([800,600])
+keep_going = True
+g = (0,255,0)
+radius = 50
+while keep_going:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            keep_going = False
+    pygame.draw.circle(screen,g,(100,100),radius)
+    pygame.display.update()
+
+pygame.quit()    
             
