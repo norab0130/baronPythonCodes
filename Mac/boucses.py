8caeedf68674006dc807a1d491fd92f9c0d597e9
aaronliu0130 <aaronliu0130@outlook.com>
2019-03-03 15:26:15 +0800

    Update Mac Codes

--- Mac/boucses.py
+++ Mac/boucses.py
@@ -4,15 +4,8 @@
 import pygame
 pygame.init()
 screen = pygame.display.set_mode([800,600])
-keep_going = True
-g = (0,255,0)
-radius = 50
-while keep_going:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            keep_going = False
-    pygame.draw.circle(screen,g,(100,100),radius)
-    pygame.display.update()
-
-pygame.quit()    
+keep_going= True
+pic = pygame.image.load("crasysmile.bmp")
+colerkey = pic.get_at((0,0))
+pic.set_colerkey(colerkey)    
             
