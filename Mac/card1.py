b5c9be5fafe800a00408727093af3df93c2b01b6
aaronliu0130 <aaronliu0130@outlook.com>
2019-03-04 19:15:52 +0800

    Update card1.py

--- Mac/card1.py
+++ Mac/card1.py
@@ -5,49 +5,91 @@ Created on Feb 16, 2019
 '''
 
 import random
+import turtle
 suits = ["clubs", "diamonds", "heart", "spades"]
 fases = ["two", "three", "four", "five", "six", "seven", "eight",
          "nine", "ten", "jack", "queen", "king", "ace"]
 playAgain = True;
-mC = 0;
-yC = 0;
+
+tm = 0;
+ty = 0;
 while playAgain:
-    myace = random.choice(fases)
-    uts = random.choice(suits)
-    yourace = random.choice(fases)
-    youruts = random.choice(suits)
-    mycard = 50
-    yourcard = 50
-    print("i get", myace, "of", uts)
-    print("you got", yourace, "of", youruts) 
-    if myace + uts == yourace + youruts:
-        print("its a tie")
-        mycard = mycard - 1
-        yourcard = yourcard - 1
-    if myace + uts > yourace + youruts:
-        print("you won")
-        yC += 1
+    tC = 0
+    mC = 0;
+    yC = 0;
+    while tC < 50:
+        myace = random.choice(fases)
+        uts = random.choice(suits)
+        yourace = random.choice(fases)
+        youruts = random.choice(suits)
+        mycard = 50
+        yourcard = 50
+        print("i get", myace, "of", uts)
+        print("you got", yourace, "of", youruts) 
+        if myace + uts == yourace + youruts:
+            print("its a tie")
+            mycard = mycard - 1
+            yourcard = yourcard - 1
+        if myace + uts > yourace + youruts:
+            print("you won")
+            yC += 1
+            yourcard = yourcard - 2
+            mycard += 1
+        else:
+            print("i won")
+            mC += 1
+            yourcard += 1
+            mycard = mycard - 2
+        if mycard == 0:
+            print("i lose you win")
+        if yourcard == 0:
+            print("i win you lose")
+        tC += 1
+    print("SCOREBOARD:")
+    print("WE HAVE PLAYED")
+    print(tC)
+    print("ROUNDS")
+    choices = [0, 1];
+    order = 0
+    if order == random.choice(choices):
+        print("MY SCORE ISSSSSSSSSSSSS...")
+        print(str(mC) + "!")
+        print("AAAAAAAAAAAAAND...")
+        print("YOUR")
+        print("SCORE")
+        print("ISSSSSSSSSSS...")
+        print("suspense")
+        print("suspense")
+        print("suspense")
+        print("YOUR SCORE IS " + str(yC) + "!")
     else:
-        print("i won")
-        mC += 1
-    if mycard == 0:
-        print("i lose you win")
-    if yourcard == 0:
-        print("i win you lose")
+        print("YOUR SCORE ISSSSSSSSSSSSS...")
+        print(str(yC) + "!")
+        print("AAAAAAAAAAAAAND...")
+        print("MY")
+        print("SCORE")
+        print("ISSSSSSSSSSS...")
+        print("suspense")
+        print("suspense")
+        print("suspense")
+        print("MY SCORE IS " + str(mC) + "!")
+    print("I WIN!!!!YAYYYAYÅÁAYAYAYAY!" if mC > yC else "you win............nooooooooooo")
+    print("Anyways,Continue!")
     pas = ""
+    tm+=mC;ty+=yC
     while pas != "y" and pas != "n":
         pas = input("Do you want to play again(y/n)?")
     if pas == "n":
         playAgain = False;
 print("SCOREBOARD:")
 print("WE HAVE PLAYED")
-print(yC+mC)
+print(yC + mC)
 print("ROUNDS")
 choices = [0, 1];
 order = 0
 if order == random.choice(choices):
     print("MY SCORE ISSSSSSSSSSSSS...")
-    print(str(mC) + "!")
+    print(str(tm) + "!")
     print("AAAAAAAAAAAAAND...")
     print("YOUR")
     print("SCORE")
@@ -55,10 +97,10 @@ if order == random.choice(choices):
     print("suspense")
     print("suspense")
     print("suspense")
-    print("YOUR SCORE IS " + str(yC) + "!")
+    print("YOUR SCORE IS " + str(ty) + "!")
 else:
     print("YOUR SCORE ISSSSSSSSSSSSS...")
-    print(str(yC) + "!")
+    print(str(ty) + "!")
     print("AAAAAAAAAAAAAND...")
     print("MY")
     print("SCORE")
@@ -66,6 +108,6 @@ else:
     print("suspense")
     print("suspense")
     print("suspense")
-    print("MY SCORE IS " + str(mC) + "!")
-print("I WIN!!!!YAYYYAYÅÁAYAYAYAY!" if mC > yC else "you win............nooooooooooo")
+    print("MY SCORE IS " + str(tm) + "!")
+print("I WIN!!!!YAYYYAYÅÁAYAYAYAY!" if tm > ty else "you win............nooooooooooo")
 print("Anyways,Goodbye!")
