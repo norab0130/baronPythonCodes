7e918b611f3f09c66b1eb25ded4fbfb19d64fab4
Aaron Liu <aaronliu0130@126.com>
2020-02-25 22:23:19 +0800

    Added one code, delete cache
    There. Will fork and delete later.

--- Win/bounce1.py
+++ Win/bounce1.py
@@ -1,17 +1,29 @@
 '''
-Created on Jan 31, 2019
+Created on 31, 
 
-@author: Aaron Liu
+@author: aron Liu
 '''
-import random
-dice=[0,0,0,0,0]
-for i in range(5):
-    dice[i]= random.randint(1,6)
-print("you rolled ",dice)
-dice.sort()
-if dice[0] == dice[4]:
-    print("yahitzee!")
-if dice[0] == dice[3] or dice[1] == dice[4]:
-    print("four of a kind!")
-if dice[0]== dice[2] or dice[1]==dice[3] or dice[2]== dice[4]:
-    print("three of a kind")    
+'''
+Aaron's comments: 1.Why'd you sign your name as "aron" Liu? 2. On the
+first line, you should add a semicolon and space (or even better, a 
+newline!) after the text demanding input. That's good practice, my 100th 
+middle name. 3. You should say what out of what to select. With a title
+called "bounce1.py" nobody knows what to expect. Don't worry about all
+the tornadoes, alarms, etc. Those are "easter eggs".
+'''
+le = input("select one")
+while le=="alarm":
+    print("its just a dream! wake up@#$#%@#$%$%#$")
+    print("now your console gone completly wrong!")
+if le=="tornado":
+    print("ATTENTION! DISASTER WILL COME IN 1...SECOND!")
+    print("i select alarm!you lose!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    quit()
+if le == "scissors":
+    print("i select rock! ya los!")
+if le == "rock":
+    print("supercalifragilisticexpalidocius! youlose i choose tornado!!!!! ")
+if le == "paper":
+    print("i choose scissor!yer chance was bad ")
+if le != "rock" and le != "scissors" and le != "paper" and le!="tornado"and le!="alarm":
+    print("SCRAM!")
